c152ed91a9eeaa0ac9989d61b7989beeff34a548
Jordan Sleeper <findingDavincii@gmail.com>
2016-10-26 15:07:01 -0400

    riku general setup

---
+++ code/ri/riku.php
@@ -0,0 +1,15 @@
+<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1.0">
+<title>default title</title>
+<link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+
+
+
+
+
+
+
+<script src="scripts/scripts.js"></script>
+</body></html>
